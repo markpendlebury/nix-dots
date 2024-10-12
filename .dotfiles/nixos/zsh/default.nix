faa37b2f1409129409e01bcdfc808e269cc875b7
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2024-10-13 00:20:02 +0100

    Fixing an issue with zsh

---
+++ .dotfiles/nixos/zsh/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      rebuild = "sudo nixos-rebuild switch";
+    };
+
+    ohMyZsh = {
+      enable = true;
+      custom = "~/.config/zsh/";
+      theme = "gruvbox";
+    };
+  };
+
+  
+}
