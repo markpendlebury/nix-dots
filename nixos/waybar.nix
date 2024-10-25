a029a2b5519a68aed5a81e5881dfb74ef34a0168
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2024-10-25 22:03:19 +0100

    waybar config

--- nixos/waybar.nix
+++ nixos/waybar.nix
@@ -4,4 +4,7 @@
   programs.waybar = {
     enable = true;
   };
+
+  home.file."/etc/xdg/waybar/config.jsonc".source = ../config/waybar/config.jsonc;
+  home.file."./config/waybar/style.css".source = ../config/waybar/style.css;
 }
